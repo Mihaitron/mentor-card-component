3bbc644560f2ee286643a682e329bdd6d23b8ca5
Mihai Popa <mihai.popa@tractorsetgo.com>
2022-10-22 10:16:48 +0300

    Add required fonts and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Card from "./Card/Card";
+import Card from "./components/Card/Card";
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Card />
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+          <Card />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import "../../fonts/Montserrat-Medium.ttf";
+import "../../fonts/Montserrat-Bold.ttf";
+import "../../fonts/Fraunces_144pt-Bold.ttf";
+
+import './Card.css'
+
+const Card = (props) => {
+    return (
+        <>
+            Preview
+
+            Gabrielle Essence Eau De Parfum
+
+            A floral, solar and voluptuous interpretation composed by Olivier Polge,
+            Perfumer-Creator for the House of CHANEL.
+
+            $149.99
+            $169.99
+
+            Add to Cart
+
+            <div className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                Coded by <a href="src/components/Card/Card#">Mihai Andrei Popa</a>.
+            </div>
+        </>
+    );
+};
+
+export default Card;
